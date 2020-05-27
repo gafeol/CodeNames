6ad7c9faf35e3787995788d055ee2e522191ab91
gafeol <gabfeol@gmail.com>
2020-05-27 11:40:14 -0300

    Adds React front

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  useEffect(() => {
+    // Testing API request
+    fetch('/api/test')
+      .then(res => res.json()) // Gets only the json from the response
+      .then(({answer}) => console.log(answer));
+  });
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
+const port = (process.env.PORT || 5000);
+
+
+app.get('/api/test', (req, res) => {
+    console.log("API working!");
+    res.json({'answer': "API working!"});
+});
+
+app.get('*', (req, res) => {
+    console.log("redirecting to react...")
+    res.sendFile(path.join(__dirname + '/client/public/index.html'));
+});
+
+io.on('connection', socket => {
+    console.log("Client connected!");
+    socket.on('add user', username => {
+        socket.username = username;
+    });
+    socket.broadcast('user joined', {
+        username: socket.username,
+    });
+});
+
+server.listen(port, () => console.log(`Listening on ${port}`));
