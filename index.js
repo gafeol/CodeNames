34b98cb11ffec751da3b778121848e20037e1f44
gafeol <gabfeol@gmail.com>
2020-05-27 11:54:39 -0300

    Adds rendering of client build folder pages

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(server);
 const path = require('path');
 const port = (process.env.PORT || 5000);
 
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/api/test', (req, res) => {
     console.log("API working!");
     res.json({'answer': "API working!"});
